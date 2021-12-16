6efcc3d39d44e78205c461b5c1f6e4cd662a952e
Zhenhua Yang <huaouo@gmail.com>
2021-12-17 00:04:02 +0800

    Tailor 3rd libraries

--- package.sh
+++ package.sh
@@ -6,8 +6,7 @@ cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -G Ninja
 cmake --build build -j -- -v | cut -d' ' -f2- | tee compile.sh
 sed -i -e 's|/usr/bin/cmake -E||g' compile.sh
 find build ! -type d -exec rm '{}' \;
-find build -type d -empty -exec touch {}/.gitkeep \;
 
 # Zip the source code
 rm -f tdsql.zip
-zip -r -9 tdsql.zip 3rd build src compile.sh make.sh start.sh
+zip -r tdsql.zip 3rd build src compile.sh make.sh start.sh
